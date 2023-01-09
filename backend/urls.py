7741cdf29611d3aa31abd08f9e46775079139896
Zikun Cui <1013125203@qq.com>
2023-01-09 02:02:57 -0800

    1.Update frontend 2.Exempt CSRF in Debug mode

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, re_path
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
+from django.conf import settings
 from django.views.generic import TemplateView
 from graphene_file_upload.django import FileUploadGraphQLView
 
 app_name = 'backend'
 
+graphql_view = FileUploadGraphQLView.as_view(graphiql=True)
+
 urlpatterns = [
-    path("graphql",  FileUploadGraphQLView.as_view(graphiql=True)),
+    path("graphql", csrf_exempt(graphql_view) if settings.DEBUG else graphql_view),
     re_path(r".*", ensure_csrf_cookie(TemplateView.as_view(template_name="backend/index.html")), name="main"),
 ]
