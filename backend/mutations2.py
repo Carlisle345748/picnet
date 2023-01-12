72c867ddb7d7df3c9720f3906d261ed11e837a3d
Zikun Cui <1013125203@qq.com>
2023-01-12 10:47:13 -0800

    reimplement upload photo and avatar

--- backend/mutations2.py
+++ backend/mutations2.py
@@ -1,18 +1,21 @@
-from typing import Optional, cast, Type
+from typing import Optional, cast, Type, List
 
-from algoliasearch_django import update_records
+from algoliasearch_django import update_records, save_record
+from algoliasearch_django.decorators import disable_auto_indexing
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
-from django.db import transaction
+from django.db import transaction, IntegrityError
+from graphql import GraphQLError
+from strawberry.file_uploads import Upload
 from strawberry.types import Info
 from strawberry_django import auth
-from strawberry_django_plus import gql, relay
+from strawberry_django_plus import gql
 from strawberry_django_plus.mutations import resolvers
 from strawberry_django_plus.relay import GlobalID
 
-from .directive import IsAuthenticated
-from .models import Profile, Photo, Comment
-from .types2 import UserType, CommentType, PhotoType
+from .errors import ERR_USERNAME_EXIST
+from .models import Profile, Photo, Comment, PhotoTag, Feed
+from .types2 import UserType, CommentType, PhotoType, ProfileType
 
 UserModel = cast(Type[User], get_user_model())
 
@@ -26,11 +29,11 @@ class UpdateFollowerResult:
 # noinspection PyShadowingBuiltins
 @gql.type
 class Mutation:
-    login: UserType = auth.login()
+    login: Optional[UserType] = auth.login()
 
     logout = auth.logout()
 
-    @relay.input_mutation(directives=[IsAuthenticated()])
+    @gql.django.input_mutation(handle_django_errors=False)
     @transaction.atomic
     def create_user(
             self,
@@ -41,17 +44,20 @@ class Mutation:
             email: Optional[str] = "",
             description: Optional[str] = ""
     ) -> UserType:
-        user = UserModel.objects.create_user(
-            username=username,
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            password=password,
-        )
-        Profile.objects.create(user=user, description=description)
-        return cast(UserType, user)
+        try:
+            user = UserModel.objects.create_user(
+                username=username,
+                email=email,
+                first_name=first_name,
+                last_name=last_name,
+                password=password,
+            )
+            Profile.objects.create(user=user, description=description)
+            return cast(UserType, user)
+        except IntegrityError:
+            raise GraphQLError(message="username already exist", extensions=ERR_USERNAME_EXIST)
 
-    @relay.input_mutation
+    @gql.django.input_mutation(handle_django_errors=False)
     @transaction.atomic
     def create_comment(self, info: Info, photo_id: GlobalID, comment: str) -> CommentType:
         new_comment = Comment.objects.create(
@@ -66,10 +72,10 @@ class Mutation:
         )
         return cast(CommentType, new_comment)
 
-    @relay.input_mutation
+    @gql.django.input_mutation(handle_django_errors=False)
     @transaction.atomic
-    def update_profile(self, info: Info, id: GlobalID, first_name: str, last_name: str, description: str) -> UserType:
-        user = id.resolve_node(info, ensure_type=UserModel)
+    def update_profile(self, info: Info, first_name: str, last_name: str, description: str) -> UserType:
+        user = info.context.request.user
         user.first_name = first_name
         user.last_name = last_name
         user.profile.description = description
@@ -77,7 +83,7 @@ class Mutation:
         user.profile.save()
         return cast(UserType, user)
 
-    @relay.input_mutation
+    @gql.django.input_mutation(handle_django_errors=False)
     @transaction.atomic
     def update_photo_like(self, info: Info, photo_id: GlobalID, like: bool) -> PhotoType:
         user = info.context.request.user
@@ -88,7 +94,7 @@ class Mutation:
             photo.user_like.remove(user)
         return cast(PhotoType, photo)
 
-    @relay.input_mutation
+    @gql.django.input_mutation(handle_django_errors=False)
     @transaction.atomic
     def update_follower(self, info: Info, user_id: GlobalID, follow: bool) -> UpdateFollowerResult:
         logged_in_user: User = info.context.request.user
@@ -101,12 +107,12 @@ class Mutation:
             follow_user.profile.follower.remove(logged_in_user)
         return UpdateFollowerResult(user=logged_in_user, follow_user=follow_user)
 
-    @relay.input_mutation
+    @gql.django.input_mutation(handle_django_errors=False)
     def delete_photo(self, info: Info, id: GlobalID) -> PhotoType:
         photo = Photo.objects.get(pk=id.node_id)
         return cast(PhotoType, resolvers.delete(info, photo))
 
-    @relay.input_mutation
+    @gql.django.input_mutation(handle_django_errors=False)
     def delete_comment(self, info: Info, id: GlobalID) -> CommentType:
         comment = Comment.objects.get(pk=id.node_id)
         comment = resolvers.delete(info, comment)
@@ -114,3 +120,43 @@ class Mutation:
         qs = Photo.objects.filter(pk=comment.photo_id)
         update_records(model=Photo, qs=qs, photo_comments=qs[0].photo_comments())
         return cast(CommentType, comment)
+
+    @gql.django.input_mutation(handle_django_errors=False)
+    def upload_avatar(self, info: Info, avatar: Upload) -> ProfileType:
+        profile = info.context.request.user.profile
+        profile.avatar = avatar
+        profile.save()
+        return cast(ProfileType, profile)
+
+    @gql.django.input_mutation(handle_django_errors=False)
+    @disable_auto_indexing(model=Photo)
+    @transaction.atomic
+    def upload_photo(
+            self,
+            info: Info,
+            photo: Upload,
+            description: str,
+            location: str,
+            tags: List[str],
+            ratio: float
+    ) -> PhotoType:
+        user = info.context.request.user
+        photo = Photo(
+            file_name=photo,
+            ratio=ratio,
+            user=user,
+            description=description,
+            location=location
+        )
+        photo.save()
+
+        for tagName in tags:
+            tag, _ = PhotoTag.objects.get_or_create(tag=tagName)
+            photo.tags.add(tag)
+
+        save_record(photo)
+
+        followers = user.profile.follower.all()
+        feeds = [Feed(user=follower, photo=photo) for follower in followers]
+        Feed.objects.bulk_create(feeds)
+        return cast(PhotoType, photo)
